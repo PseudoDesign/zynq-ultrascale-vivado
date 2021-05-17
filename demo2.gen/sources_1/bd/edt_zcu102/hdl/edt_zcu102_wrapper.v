94fc0e3bb58f5d0a37753930fca904f6e379e7d2
Adam <>
2021-05-16 22:12:48 -0600

    built bitstream

---
+++ demo2.gen/sources_1/bd/edt_zcu102/hdl/edt_zcu102_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+//Date        : Sun May 16 21:56:21 2021
+//Host        : Kylo running 64-bit major release  (build 9200)
+//Command     : generate_target edt_zcu102_wrapper.bd
+//Design      : edt_zcu102_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module edt_zcu102_wrapper
+   ();
+
+
+  edt_zcu102 edt_zcu102_i
+       ();
+endmodule
